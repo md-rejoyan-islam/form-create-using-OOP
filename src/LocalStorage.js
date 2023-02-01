b6920804ad13540864cbb4beadbeb1f30e4a95e2
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-02-02 00:27:01 +0600

    object oriented form

--- src/LocalStorage.js
+++ src/LocalStorage.js
@@ -1,12 +1,12 @@
-class LocalStorage{
-   static save=(key,value)=>{
-    let data=[]
+class LocalStorage {
+  static save = (key, value) => {
+    let data = [];
     if (localStorage.getItem(key)) {
-      data= JSON.parse(localStorage.getItem(key));
+      data = JSON.parse(localStorage.getItem(key));
     }
-    data.push(JSON.stringify(value))
-        localStorage.setItem(key, JSON.stringify(data));
-    }
+    data.push(JSON.stringify(value));
+    localStorage.setItem(key, JSON.stringify(data));
+  };
 }
 
 export default LocalStorage;
